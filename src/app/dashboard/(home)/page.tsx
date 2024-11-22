ee2776d934eb53a084934b86304f747283f99008
kingsonseang <kingsonseang@gmail.com>
2024-11-22 12:31:20 +0100

    fix: update tickets checked in

--- src/app/dashboard/(home)/page.tsx
+++ src/app/dashboard/(home)/page.tsx
@@ -16,7 +16,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Edit } from "lucide-react";
+import { Edit, HandCoins, LogIn } from "lucide-react";
 import { formatEventDate, getEventTimeRange } from "@/lib/utils";
 import { redirect, RedirectType } from "next/navigation";
 
@@ -157,7 +157,7 @@ export default async function Page() {
                     />
                     <path
                       d='M18 17C18 17.2652 17.8946 17.5196 17.7071 17.7071C17.5196 17.8946 17.2652 18 17 18C16.7348 18 16.4804 17.8946 16.2929 17.7071C16.1054 17.5196 16 17.2652 16 17C16 16.7348 16.1054 16.4804 16.2929 16.2929C16.4804 16.1054 16.7348 16 17 16C17.2652 16 17.5196 16.1054 17.7071 16.2929C17.8946 16.4804 18 16.7348 18 17ZM18 13C18 13.2652 17.8946 13.5196 17.7071 13.7071C17.5196 13.8946 17.2652 14 17 14C16.7348 14 16.4804 13.8946 16.2929 13.7071C16.1054 13.5196 16 13.2652 16 13C16 12.7348 16.1054 12.4804 16.2929 12.2929C16.4804 12.1054 16.7348 12 17 12C17.2652 12 17.5196 12.1054 17.7071 12.2929C17.8946 12.4804 18 12.7348 18 13ZM13 17C13 17.2652 12.8946 17.5196 12.7071 17.7071C12.5196 17.8946 12.2652 18 12 18C11.7348 18 11.4804 17.8946 11.2929 17.7071C11.1054 17.5196 11 17.2652 11 17C11 16.7348 11.1054 16.4804 11.2929 16.2929C11.4804 16.1054 11.7348 16 12 16C12.2652 16 12.5196 16.1054 12.7071 16.2929C12.8946 16.4804 13 16.7348 13 17ZM13 13C13 13.2652 12.8946 13.5196 12.7071 13.7071C12.5196 13.8946 12.2652 14 12 14C11.7348 14 11.4804 13.8946 11.2929 13.7071C11.1054 13.5196 11 13.2652 11 13C11 12.7348 11.1054 12.4804 11.2929 12.2929C11.4804 12.1054 11.7348 12 12 12C12.2652 12 12.5196 12.1054 12.7071 12.2929C12.8946 12.4804 13 12.7348 13 13ZM8 17C8 17.2652 7.89464 17.5196 7.70711 17.7071C7.51957 17.8946 7.26522 18 7 18C6.73478 18 6.48043 17.8946 6.29289 17.7071C6.10536 17.5196 6 17.2652 6 17C6 16.7348 6.10536 16.4804 6.29289 16.2929C6.48043 16.1054 6.73478 16 7 16C7.26522 16 7.51957 16.1054 7.70711 16.2929C7.89464 16.4804 8 16.7348 8 17ZM8 13C8 13.2652 7.89464 13.5196 7.70711 13.7071C7.51957 13.8946 7.26522 14 7 14C6.73478 14 6.48043 13.8946 6.29289 13.7071C6.10536 13.5196 6 13.2652 6 13C6 12.7348 6.10536 12.4804 6.29289 12.2929C6.48043 12.1054 6.73478 12 7 12C7.26522 12 7.51957 12.1054 7.70711 12.2929C7.89464 12.4804 8 12.7348 8 13Z'
-                      fill='black'
+                      fill='currentColor'
                     />
                   </svg>
 
@@ -225,6 +225,21 @@ export default async function Page() {
                 >
                   {res?.data?.event?.location}
                 </Link>
+              </div>
+
+              <div className='flex gap-3 items-start hover:text-dark-blue/60 transition ease-linear duration-200 cursor-pointer font-semibold'>
+                <HandCoins strokeWidth={1.5} className='size-5 flex-shrink-0' />
+                <p>
+                  Revenue: ₦{" "}
+                  {parseFloat(
+                    res?.data?.totalRevenue?.toFixed(2) ?? "0"
+                  ).toLocaleString()}
+                </p>
+              </div>
+
+              <div className='flex gap-3 items-start hover:text-dark-blue/60 transition ease-linear duration-200 cursor-pointer font-semibold'>
+                <LogIn strokeWidth={1.5} className='size-5 flex-shrink-0' />
+                <p>Checkins: {res?.data?.event?.total_checked_in}</p>
               </div>
             </CardFooter>
           </div>
